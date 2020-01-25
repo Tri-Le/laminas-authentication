dd5674d7c5f8bed5568427280e7532a4fe6709b0
tri-le <ledinhtri@gmail.com>
2020-01-26 01:09:34 +0700

    Add service factory

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,9 +4,30 @@
 namespace TriLe\Authentication\Controller;
 
 
+use Laminas\Authentication\AuthenticationService;
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\Mvc\MvcEvent;
+use Laminas\View\Model\ViewModel;
+use TriLe\Authentication\Form\Login;
 
 class LoginController extends AbstractActionController
 {
+    protected $service;
 
+    public function onDispatch(MvcEvent $e)
+    {
+        $this->service = $e->getApplication()->getServiceManager()->get(AuthenticationService::class);
+        $this->layout('layout/login');
+        return parent::onDispatch($e);
+    }
+
+    public function indexAction()
+    {
+        $form = new Login('LoginForm');
+        $form->init();
+
+        return new ViewModel([
+            'form' => $form
+        ]);
+    }
 }
