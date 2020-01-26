43a38fba9d5a59c6d92834ac617316faf4bba197
tri.le <ledinhtri@gmail.com>
2020-01-27 00:38:07 +0700

    Add redirect url

--- src/Controller/SuccessController.php
+++ src/Controller/SuccessController.php
@@ -3,7 +3,21 @@ namespace TriLe\Authentication\Controller;
 
 
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
 
 class SuccessController extends AbstractActionController {
+	public function indexAction() {
+		$dest = $this->params()->fromQuery('_dest');
+		if (!$dest) {
+			$dest = $this->params()->fromRoute('_dest');
 
+			if (!$dest) {
+				$dest = $this->url()->fromRoute('home');
+			}
+		}
+
+		return new ViewModel([
+			'destination' => $dest
+		]);
+	}
 }
